00d383108916ba6e6448644a97ee3dd7a44beece
Skeo <skeo051600@gmail.com>
2025-01-04 22:37:05 -0600

    complete

---
+++ src/api/jokeApi.js
@@ -0,0 +1,18 @@
+const endpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart'
+
+const getJoke = () => new Promise((resolve, reject) => {
+  fetch(endpoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      resolve(data)
+    }
+    )
+    .catch(reject)
+})
+
+export default getJoke;
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,34 @@
+"use client"
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import getJoke from "@/api/jokeApi";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [joke, setJoke] = useState({})
+  const [buttonText, setButtonText] = useState('click for a new joke')
+  const [showSetup, setShowSetup] = useState(true);
+
+  const getTheJoke = () => {
+    if (showSetup) {
+      setShowSetup(false);
+    } else {
+      getJoke().then((newJoke) => {
+        setJoke(newJoke);
+        setShowSetup(true);
+        console.log(newJoke);
+      });
+    }
+  }
+
+  return (
+    <div>
+      <div id="joke-container">
+        {showSetup ? joke.setup : joke.delivery}
+      </div>
+      <button onClick={getTheJoke} id="joke-button">{buttonText}</button>
     </div>
   );
 }
